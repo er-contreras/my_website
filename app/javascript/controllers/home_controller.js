b4639483a0e21c8889847b448eaf74b605f921b6
Erick Contreras <er.contreras@hotmail.com>
2024-03-07 17:39:52 -0600

    Add color on the current link path

--- app/javascript/controllers/home_controller.js
+++ app/javascript/controllers/home_controller.js
@@ -1,5 +1,14 @@
-import { Controller } from "@hotwired/stimulus"
-
+import {Controller} from "@hotwired/stimulus"
+//
 export default class extends Controller {
-
+//   static targets = ["link"];
+//
+//   toggle(event) {
+//     this.linkTargets.forEach((link) => {
+//       console.log(event.target);
+//
+//       // if event target a href equal to current page url then add active class
+//       console.log(window.location.pathname);
+//     });
+//   }
 }
