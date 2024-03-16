17c2897149a1af6371ebbf396423d55a319b9a13
Erick Contreras <er.contreras@hotmail.com>
2024-03-16 13:08:47 -0600

    Add eventListener

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -3,6 +3,11 @@ import {Controller} from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["modal", "content"]
 
+  connect() {
+    this.element.addEventListener("click", this.open.bind(this));
+    this.element.addEventListener(("touchstart"), this.open.bind(this));
+  }
+
   open(event) {
     // Change display none to block
     document.getElementById("modal").style.display = "block";
