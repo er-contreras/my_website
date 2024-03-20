4a38ddb2a232b2464d5cb33203e6d66da27e20ea
Erick Contreras <er.contreras@hotmail.com>
2024-03-19 20:14:24 -0600

    Move colors script into stimulus controller

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -1,6 +1,20 @@
 import {Controller} from "@hotwired/stimulus"
 //
 export default class extends Controller {
+  connect() {
+    const colorsArray = ["tomato", "orange", "yellow", "green", "blue", "indigo", "violet", "purple", "pink", "red", "white"];
+    let i = 0;
+
+    function changeColor() {
+      document.getElementById("change-color").style.color = colorsArray[Math.floor(Math.random() * colorsArray.length)];
+      i = (i + 1) % colorsArray.length;
+    }
+
+    setInterval(function () {
+      changeColor();
+    }, 1200);
+  }
+
   open(event) {
     event.preventDefault();
     console.log("click open");
